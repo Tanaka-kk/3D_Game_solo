70af8d5cdc79e7fc06332aa6a81b8a83b6151d2e
rokuryou <chonlawit.l@kkumail.com>
2025-09-29 13:32:29 +0700

    name Color

--- Scripts/GameUI.gd
+++ Scripts/GameUI.gd
@@ -3,8 +3,9 @@ extends Control
 # ---------- VARIABLES ---------- #
 
 @onready var coinsLabel = $CoinsLabel
-
+@onready var nameLabel = $NameLabel
 # ---------- FUNCTIONS ---------- #
 
 func _process(_delta):
 	coinsLabel.text = "x %d" % GameManager.score # Set the coin label text to the score variable
+	nameLabel.text = "นายชลวิชญ์ ลิลาศ 663380260-7"
